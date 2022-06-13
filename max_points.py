acc37da2842e09bfd8e4a08125e1e190f6ea89ef
leverhea <leverhea@gmail.com>
2022-06-13 20:30:52 +0000

    remove node

--- max_points.py
+++ max_points.py
@@ -12,7 +12,7 @@
 
 
 class Checkpoint():
-    def __init__(self, points, children):
+    def __init__(self, points, children=[]):
         self.points = points
         self.children = children or []
 
@@ -53,19 +53,22 @@ game = Checkpoint(
 
 
 class Traverser():
-    def __init__(self, game):
+    def __init__(self):
         self.max_score = 0
-        return self.getMaxScore(game)
 
-    def getMaxScore(checkpoint):
+    def getMaxScore(self, checkpoint, current_score):
         current_score += checkpoint.points
         if not checkpoint.children:
             self.max_score = max(self.max_score, current_score)
         else:
-            return self.getMaxScore(checkpoint)
+            for child in checkpoint.children:
+                self.getMaxScore(child, current_score)
+        return self.max_score
 
 
-print(Traverser(game))
+t = Traverser()
+
+print(t.getMaxScore(game, 0))
 
 
 
